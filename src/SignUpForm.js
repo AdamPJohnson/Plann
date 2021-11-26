0f427af2ca9464d01bdd82fcca336131945a119c
Adam Johnson <adam.p.johnson.dc@gmail.com>
2021-11-26 15:31:40 -0800

    implemented basic signup modal, refactored form

--- src/SignUpForm.js
+++ src/SignUpForm.js
@@ -2,9 +2,10 @@ import React, { useState } from "react";
 import useForm from "./useForm";
 import Button from "react-bootstrap/Button";
 import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { ToggleButton, ToggleButtonGroup } from "@mui/material/";
-
+import Modal from "react-modal";
+///////define modal app element
 function SignUpForm({ setIsLoggedIn, isOrg, setIsOrg, setUser }) {
   const [formData, onChange] = useForm({
     username: "",
@@ -15,6 +16,12 @@ function SignUpForm({ setIsLoggedIn, isOrg, setIsOrg, setUser }) {
   });
   const navigate = useNavigate();
   const [errorMessage, setErrorMessage] = useState("");
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const checkUsernameAvailability = (e) => {
+    e.preventDefault();
+
+    setModalIsOpen(true);
+  };
   const onSubmit = (e) => {
     e.preventDefault();
 
@@ -35,6 +42,17 @@ function SignUpForm({ setIsLoggedIn, isOrg, setIsOrg, setUser }) {
     console.log(isOrg);
     setIsOrg((prev) => !prev);
   };
+  const customStyles = {
+    content: {
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+      borderRadius: "10px",
+      border: "none",
+      boxShadow: "1px 1px 15px rgba(0,0,0,0.3)",
+    },
+  };
   return (
     <form id="loginForm">
       <h2>Sign Up...</h2>
@@ -53,36 +71,85 @@ function SignUpForm({ setIsLoggedIn, isOrg, setIsOrg, setUser }) {
       {isOrg && (
         <>
           <label htmlFor="orgName">Organization Name</label>
-          <input onChange={onChange} type="text" name="orgName" />
+          <input
+            onChange={onChange}
+            type="text"
+            name="orgName"
+            value={formData.orgName}
+          />
         </>
       )}
-      <label htmlFor="email">Email</label>
-      <input onChange={onChange} type="text" name="email" />
-      <label htmlFor="zip">Zip Code</label>
-      <input onChange={onChange} type="text" name="zip" />
       <label htmlFor="username">Username</label>
-      <input onChange={onChange} type="text" name="username" />
-      <label htmlFor="password">Password</label>
-      <input onChange={onChange} type="password" name="password" />
-      <Link
-        to="/"
-        style={{
-          textDecoration: "none",
-          width: "100%",
-          display: "flex",
-          justifyContent: "center",
-        }}
+      <input
+        onChange={onChange}
+        type="text"
+        name="username"
+        value={formData.username}
+      />
+      <Modal
+        id="signUpModal"
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        style={customStyles}
       >
+        {isOrg && (
+          <>
+            <label htmlFor="orgName">Organization Name</label>
+            <input
+              onChange={onChange}
+              type="text"
+              name="orgName"
+              value={formData.orgName}
+            />
+          </>
+        )}
+        <label htmlFor="username">Username</label>
+        <input
+          onChange={onChange}
+          type="text"
+          name="username"
+          value={formData.username}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          onChange={onChange}
+          type="password"
+          name="password"
+          value={formData.password}
+        />
+        <label htmlFor="zip">Zip Code</label>
+        <input
+          onChange={onChange}
+          type="text"
+          name="zip"
+          value={formData.zip}
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          onChange={onChange}
+          type="text"
+          name="email"
+          value={formData.email}
+        />
         <Button
           variant="outline-dark"
           className="mySubmitButton"
           onClick={onSubmit}
           type="submit"
         >
-          Sign Up
+          Submit
         </Button>
-      </Link>
-      <div className="errorMessage">{errorMessage}</div>
+        <div className="errorMessage">{errorMessage}</div>
+      </Modal>
+
+      <Button
+        variant="outline-dark"
+        className="mySubmitButton"
+        onClick={checkUsernameAvailability}
+        type="submit"
+      >
+        Sign Up
+      </Button>
     </form>
   );
 }
