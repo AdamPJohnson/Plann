e4ec37d393c0666e29c05ba9da68f84669df00ee
Adam Johnson <adam.p.johnson.dc@gmail.com>
2022-02-15 12:54:34 -0800

    restricted Record type to string values in useForm

--- src/useForm.tsx
+++ src/useForm.tsx
@@ -2,18 +2,18 @@ import React, { useState } from "react";
 
 type formElement = HTMLInputElement | HTMLTextAreaElement;
 
-const useForm = (
-  inputs: Record<string, string | number>
-): [Record<string, string | number>, React.ChangeEventHandler<formElement>] => {
+function useForm(
+  inputs: Record<string, string>
+): [Record<string, string>, React.ChangeEventHandler<formElement>] {
   const [formData, setFormData] = useState(inputs);
   const onChange = (e: React.ChangeEvent<formElement>) => {
-    const target = e.target as formElement;
+    const target = e.target;
     const newFormData = { ...formData };
     newFormData[target.name] = e.target.value;
     setFormData(newFormData);
   };
 
   return [formData, onChange];
-};
+}
 
 export default useForm;
